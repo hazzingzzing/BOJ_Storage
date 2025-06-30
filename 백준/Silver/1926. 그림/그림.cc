ac0f058f514450a071d26efeea31786278d50ad2
hazzingzzing <hahyesung_@naver.com>
2025-06-30 15:45:41 +0900

    [Silver I] Title: 그림, Time: 24 ms, Memory: 3248 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,62 +1,76 @@
 #include <iostream>
-#include <utility>
 #include <queue>
+#include <utility>
 #include <algorithm>
+
 #define X first
 #define Y second
 
 using namespace std;
+
 int board[501][501];
 bool vis[501][501];
-int n, m;
 int dx[4] = { 1, 0, -1, 0 };
 int dy[4] = { 0, 1, 0, -1 };
+
 int main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < m; j++)
+	int input1, input2, mx = 0;
+	
+	cin >> input1 >> input2;
+
+	for (int i = 0; i < input1; i++)
+	{
+		for (int j = 0; j < input2; j++)
 		{
 			cin >> board[i][j];
 		}
+	}
 
-	int count = 0, mx = 0;
-	for (int i = 0; i < n; i++)
+	int cnt = 0;
+
+	for (int i = 0; i < input1; i++)
 	{
-		for (int j = 0; j < m; j++)
+		for (int j = 0; j < input2; j++)
 		{
 			if (board[i][j] == 0 || vis[i][j])
 				continue;
-			count++;
-			queue < pair<int, int>> Q;
-			vis[i][j] = 1;
+			queue<pair<int, int>> Q;
 			Q.push({ i,j });
+			vis[i][j] = 1;
+
+			cnt++;
 			int area = 0;
-		
+
 			while (!Q.empty())
 			{
+				auto cur = Q.front(); Q.pop();
 				area++;
-				pair <int, int> cur = Q.front(); Q.pop();
 
-				for (int i =0; i < 4; i++)
+				for (int i = 0; i < 4; i++)
 				{
 					int nx = cur.X + dx[i];
 					int ny = cur.Y + dy[i];
 
-					if (nx < 0 || ny < 0 || nx > n || ny > m)
+					if (nx < 0 || ny < 0 || nx > input1 || nx > input2)
 						continue;
-					if (vis[nx][ny] || board[nx][ny] != 1)
+					if (board[nx][ny] != 1 || vis[nx][ny])
 						continue;
-					vis[nx][ny] = 1;
 					Q.push({ nx, ny });
+					vis[nx][ny] = 1;
 				}
 			}
 			mx = max(mx, area);
 		}
 	}
-	cout << count << "\n" << mx;
+
+	cout << cnt << '\n' << mx;
+
+
+
+
 	return 0;
 }
